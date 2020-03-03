ab9e58a8627916275418b620db269f3814c83eb1
Summer8918 <yangjie8918@gmail.com>
2020-03-03 17:30:23 +0800

    add the max thread nums

--- threadpool/Main.cpp
+++ threadpool/Main.cpp
@@ -1,14 +1,18 @@
-#include "ThreadPool.hpp"
+#include "threadpool.h"
 
 int main()
 {
-    ThreadPool pool(4); //创建一个能够并发执行4个线程的线程池
+    ThreadPool pool(2,4); //创建一个能够并发执行4个线程的线程池
     
     std::vector<std::future<std::string>> results;
 
     //启动8个需要执行的线程任务
     for(int i=0;i<8;i++){
         //将并发执行任务添加到线程池中并发执行
+        //emplace_back()与push_back()的功能类似
+        //用于将元素添加到vector尾部
+        //emplace_back()省略了进行构造函数的过程
+        //就地构造，没有复制或移动操作进行
         results.emplace_back(
                 pool.enqueue([i]{   //lambda表达式
                 std::cout<<"hello "<<i<<std::endl;
--- threadpool/threadpool.h
+++ threadpool/threadpool.h
@@ -1,3 +1,5 @@
+#pragma once
+#ifndef THREAD_POOL_HPP
 #include<iostream>
 #include<vector>
 #include<queue>
@@ -6,70 +8,75 @@
 #include<thread> //this_thread::sleep_for
 #include <chrono> //std::chrono::seconds
 #include<functional> //std::bind
+#include<atomic>
+
 class ThreadPool{
 public:
     //在线程池中创建threads个工作线程
-    ThreadPool(size_t threads);
+    ThreadPool(size_t threads,size_t max_threads_nums);
 
     //向线程池中增加线程
     template<typename F,typename... Args>
         auto enqueue(F&& f,Args&&... args)
        -> std::future<typename std::result_of<F(Args...)>::type>;
-
+    //尾置返回类型
+    void addThread(size_t size);
     ~ThreadPool();
-private:
+public:
+    std::atomic<int> THREADPOOL_MAX_NUM {4};
     //需要持续追踪线程来保证可以使用join
-    std::vector<std::thread> workers;
+    std::vector<std::thread> workers;//线程池
     //任务队列
     std::queue<std::function<void()>> tasks;
 
-    //互斥锁
     std::mutex queue_mutex;  
     std::condition_variable condition;
     //停止相关
     bool stop;
+    std::atomic<int>  _idlThrNum{ 0 };  //空闲线程数量
+    int idlCount() {
+        return _idlThrNum;
+    }
+    int thrCount(){
+        return workers.size();
+    }
 };
 
- /*构造函数
- * 构造的线程池指定可并发执行的线程的数量
- * 任务的执行和结束阶段位于临界区，保证不会并发的同时启动多个需要执行的任务
+ /*
+  * 添加指定数量的线程
  */
 
-inline ThreadPool::ThreadPool(size_t threads):stop(false){
-    //启动threads数量的工作线程worker
-    for(size_t i=0;i<threads;++i){
-        std::cout<<"threads i="<<i<<std::endl;
-        workers.emplace_back(
-                //此处的lambda表达式捕获this，即线程池实例
+void ThreadPool::addThread(size_t size){
+    //增加线程数量，但不超过预定义数量 THREADPOOL_MAX_NUM
+    for(;workers.size() < THREADPOOL_MAX_NUM && size >0;--size){
+        workers.emplace_back(   //工作线程函数
                 [this]{
-                //循环避免虚假唤醒
                 for(;;){
-                //定义函数对象的容器，存储任意的返回类型为void，参数表为空的函数
-                    std::function<void()> task;
                     //临界区
+                    std::function<void()> task;  //获取一个待执行的task
                     {
-                        //创建互斥锁
                         std::unique_lock<std::mutex> lock(this->queue_mutex);
-        
-                        //阻塞当前线程，直到condition_variable被唤醒 等待lambda表达式返回true
-                        this->condition.wait(lock,
-                                [this]{ return this->stop || !this->tasks.empty();});
-
-                        if(this->stop && this->tasks.empty()){
+                        this->condition.wait(lock,[this]{return stop || !this->tasks.empty();});
+                        if(this->stop && tasks.empty()){
                             return;
                         }
-
-                        //否则就让任务队列的队首任务作为需要执行的任务出队
-                        std::cout<<"start handle task"<<std::endl;
                         task = std::move(this->tasks.front());
                         this->tasks.pop();
                     }
+                    _idlThrNum--;
                     //执行当前任务
                     task();
+                    _idlThrNum++;
                 }
                 }
-        );
+            );
+        _idlThrNum++;
     }
+}
+
+inline ThreadPool::ThreadPool(size_t threads,size_t max_threads_nums):stop(false){
+    THREADPOOL_MAX_NUM=max_threads_nums;
+    addThread(threads);
 }
 
 /*
@@ -92,13 +99,18 @@ inline ThreadPool::~ThreadPool(){
     condition.notify_all();
 
     //使所有异步线程转为同步执行
-    for(std::thread &worker:workers)
-        worker.join();
+    for(std::thread &worker:workers){
+        if(worker.joinable()){
+            worker.join();  //等待任务结束
+        }
+    }
 }
 
 /*
  * 向线程池中添加新任务
  * 支持多个入队任务参数需要使用变长模板参数
+ * 为了调度执行的任务，需要包装执行的任务，这就意味着需要对任务函数的类型进行包装、构造
+ * 临界区可以在一个作用域里边被创建，最佳实践是使用RAII的形式
  * RAII “资源获取即初始化方式”(RAII，Resource Acquisition Is Initialization)
  * */
 
@@ -106,10 +118,13 @@ template<class F,class... Args>
 auto ThreadPool::enqueue(F &&f, Args&&... args)
     -> std::future<typename std::result_of<F(Args...)>::type>
 {
+    if(stop){
+        throw std::runtime_error("enqueue on threadpool is stopped.");
+    }
     //推导任务返回类型
     using return_type = typename std::result_of<F(Args...)>::type;
 
-    //获得当前任务
+    //获得当前任务，把函数入口及参数打包绑定
     auto task = std::make_shared< std::packaged_task<return_type()> >(
             std::bind(std::forward<F>(f),std::forward<Args>(args)...)
             );
@@ -117,7 +132,7 @@ auto ThreadPool::enqueue(F &&f, Args&&... args)
     //获得std::future对象以供实施线程同步
     std::future<return_type> res = task->get_future();
     
-    //临界区
+    //临界区,添加任务到队列
     {
         std::unique_lock<std::mutex> lock(queue_mutex);
 
@@ -130,7 +145,12 @@ auto ThreadPool::enqueue(F &&f, Args&&... args)
         tasks.emplace([task]{ (*task)();});
     }
 
+    if(_idlThrNum < 1 && workers.size() < THREADPOOL_MAX_NUM){
+        addThread(1);
+    }
     //通知一个正在等待的线程
     condition.notify_one();
     return res;
 }
+
+#endif // THREAD_POOL_HPP
